2095c2ac541b4a134e8d0d20673714c8b08d542c
Lake David <ldavid432@gmail.com>
2026-02-13 09:34:24 -0600

    Add look-at to search cursor

--- src/main/java/io/hydrox/contextualcursor/ContextualCursor.java
+++ src/main/java/io/hydrox/contextualcursor/ContextualCursor.java
@@ -81,7 +81,7 @@ public enum ContextualCursor
 	PLANK("plank", "buy-plank"),
 	READ("read", "read", "story", "guide"),
 	REPORT(SpriteID.PvpwIcons.DEADMAN_EXCLAMATION_MARK_SKULLED_WARNING, "report"),
-	SEARCH("search", optionIsAnyOf("examine", "view", "look-inside", "inspect", "investigate", "peek"),
+	SEARCH("search", optionIsAnyOf("examine", "view", "look-inside", "inspect", "investigate", "peek", "look-at"),
 		hasAllOf(hasOption("check"), not(isGroundItem())), // Avoid hunter traps
 		hasAllOf(hasOption("search"), not(targetNamed("wiki"))),
 		hasAllOf(hasOption("lookup"), not(targetNamed("wiki")), not(targetStartsWith("wiki ->")))
