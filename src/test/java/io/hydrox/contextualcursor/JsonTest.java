c7f5de9fcf70fc80c4f8c14e9381eb91f4c32a95
Lake David <ldavid432@gmail.com>
2026-07-06 09:59:05 -0500

    Update json test

--- src/test/java/io/hydrox/contextualcursor/JsonTest.java
+++ src/test/java/io/hydrox/contextualcursor/JsonTest.java
@@ -1,7 +1,11 @@
 package io.hydrox.contextualcursor;
 
 import com.github.ldavid432.contextualcursor.menuentry.MenuEntryMatcher;
+import com.github.ldavid432.contextualcursor.menuentry.field.StringField;
 import com.github.ldavid432.contextualcursor.menuentry.matchers.OptionMatcher;
+import com.github.ldavid432.contextualcursor.menuentry.matchers.SimpleValueMatcher;
+import com.github.ldavid432.contextualcursor.menuentry.matchers.TargetMatcher;
+import com.github.ldavid432.contextualcursor.menuentry.predicates.StringPredicate;
 import com.github.ldavid432.contextualcursor.menuentry.predicates.ValuePredicate;
 import com.github.ldavid432.contextualcursor.serialization.adapters.MenuEntryMatcherAdapter;
 import com.github.ldavid432.contextualcursor.serialization.adapters.SpriteAdapter;
@@ -10,7 +14,7 @@ import com.github.ldavid432.contextualcursor.sprite.Sprite;
 import com.google.common.reflect.TypeToken;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import java.lang.reflect.Type;
+import java.util.Objects;
 import net.runelite.api.MenuEntry;
 import org.junit.Test;
 import org.mockito.Mockito;
@@ -27,21 +31,56 @@ public class JsonTest
 		.create();
 
 	@Test
-	public void test()
+	public void test_OptionMatcher()
 	{
-		OptionMatcher match = gson.fromJson("{\"type\": \"option\", \"predicate\": \"startsWith\", \"value\": \"bank\"}", OptionMatcher.class);
+		SimpleValueMatcher<String> match = gson.fromJson("{\"type\": \"option\", \"predicate\": \"startsWith\", \"value\": \"bank\"}", OptionMatcher.class);
 
 		assert match != null;
+		assert match instanceof OptionMatcher;
+
+		assert match.getValue() != null;
+		assert Objects.equals(match.getValue(), "bank");
+
+		assert match.getPredicate() != null;
+		assert match.getPredicate() instanceof StringPredicate;
+		assert match.getPredicate() == StringPredicate.STARTS_WITH;
+
+		assert match.getField() != null;
+		assert match.getField() instanceof StringField;
+		assert match.getField() == StringField.OPTION;
 
 		MenuEntry entry = Mockito.mock(MenuEntry.class);
-		Mockito.when(entry.getOption()).thenReturn("bank");
+		Mockito.when(entry.getOption()).thenReturn("bank chest");
 
 		assert match.matches(entry);
 
 	}
 
-	static <T> Type typeOf()
+	@Test
+	public void test_TargetMatcher()
 	{
-		return new TypeToken<T>() {}.getType();
+		SimpleValueMatcher<String> match = gson.fromJson("{\"type\": \"option\", \"predicate\": \"endsWith\", \"value\": \"goblin\"}", TargetMatcher.class);
+
+		assert match != null;
+		assert match instanceof TargetMatcher;
+
+		assert match.getValue() != null;
+		assert Objects.equals(match.getValue(), "goblin");
+
+		assert match.getPredicate() != null;
+		assert match.getPredicate() instanceof StringPredicate;
+		assert match.getPredicate() == StringPredicate.ENDS_WITH;
+
+		assert match.getField() != null;
+		assert match.getField() instanceof StringField;
+		assert match.getField() == StringField.TARGET;
+
+		MenuEntry entry = Mockito.mock(MenuEntry.class);
+		Mockito.when(entry.getTarget()).thenReturn("green goblin");
+
+		assert match.matches(entry);
+
 	}
+
+	
 }
